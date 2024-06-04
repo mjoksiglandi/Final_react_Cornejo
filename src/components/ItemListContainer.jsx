5f23107b294d9e2a95a6f8f518eddeb8e03d885c
Gormm <juan.cornejo.s@gmail.com>
2024-06-04 15:13:27 -0400

    add category fillter

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,46 +1,61 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import ItemList from "./ItemList";
-import Carousel from "./Carousel";
-import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
-import Loading from "./Loading";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ItemList from './ItemList';
+import Carousel from './Carousel';
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
+import Loading from './Loading';
 
 const ItemListContainer = () => {
-    const [items, setItems] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const { id } = useParams();
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-    useEffect(() => {
-        const fetchItems = async () => {
-            try {
-                const db = getFirestore();
-                const itemsCollection = collection(db, 'items');
-                const queryCollection = id ? query(itemsCollection, where('categoria', "==", id)) : itemsCollection;
-                const snapShot = await getDocs(queryCollection);
-                if (snapShot.size > 0) {
-                    const fetchedItems = snapShot.docs.slice(0, 8).map(item => ({ id: item.id, ...item.data() }));
-                    setItems(fetchedItems);
-                    setLoading(false);
-                }
-            } catch (error) {
-                console.error("Error fetching items:", error);
-            }
-        };
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const db = getFirestore();
+        const itemsCollection = collection(db, 'items');
+        const queryCollection = selectedCategory
+          ? query(itemsCollection, where('categoria', '==', selectedCategory))
+          : itemsCollection;
+        const snapShot = await getDocs(queryCollection);
+        if (snapShot.size > 0) {
+          const fetchedItems = snapShot.docs.slice(0, 8).map((item) => ({
+            id: item.id,
+            ...item.data(),
+          }));
+          setItems(fetchedItems);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
+    };
 
-        fetchItems();
+    fetchItems();
+  }, [id, selectedCategory]);
 
-    }, [id]);
-
-    return (
-        <>
-            {!id && <Carousel />} {}
-            <div className="container">
-                <div className="row my-5">
-                    {loading ? <Loading /> : <ItemList items={items} />}
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      {!id && <Carousel />}
+      <div className="container">
+        <div className="row my-5">
+          <select
+            onChange={(e) => setSelectedCategory(e.target.value)}
+            className="custom-select"
+            aria-label="Filter Items By Category"
+          >
+            <option value="">All Categories</option>
+            <option value="espada">Espada</option>
+            <option value="props">Props</option>
+            <option value="staff">staff</option>
+          </select>
+          {loading ? <Loading /> : <ItemList items={items} />}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ItemListContainer;
